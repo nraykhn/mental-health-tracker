1960cc28f7219f2fbc430938943c841ed11677be
nraykhn <nuralyakhairina7@gmail.com>
2024-09-11 13:51:25 +0700

    Mengerjakan Tutorial 2

--- main/models.py
+++ main/models.py
@@ -1,9 +1,9 @@
-from django.db import models
-
 # Create your models here.
 from django.db import models
+import uuid  # tambahkan baris ini di paling atas
 
 class MoodEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
